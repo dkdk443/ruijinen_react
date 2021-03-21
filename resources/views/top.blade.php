417db744d1915308764c597273605d599ff694e0
dkdk443 <demo_weeeds_470@yahoo.co.jp>
2021-03-21 23:32:36 +0900

    laravelにReactの導入

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,13 +1,25 @@
-<!DOCTYPE html>
-<html lang="ja">
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>類人猿分類</title>
-    <h1>類人猿分類</h1>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-
+    <div id="root"></div>
 </body>
 </html>
